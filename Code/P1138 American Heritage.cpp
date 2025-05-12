664e793f0341c711432dc9e8ec92778ddcf06318
yzc202403 <akerman233@163.com>
2025-05-12 09:33:45 +0800

    5/12

--- Code/P1138 American Heritage.cpp
+++ Code/P1138 American Heritage.cpp
@@ -1,3 +1,29 @@
 #include<iostream>
+#include<string>
+#include<cstdio>
+#include<string>
 using namespace std;
-
+void fun(string in , string pre)
+{
+	if(!pre.empty())
+	{
+		char root = pre[0];
+		int k = in.find(root);
+		pre.erase(pre.begin());
+		fun(in.substr(0,k),pre.substr(0,k));
+		fun(in.substr(k+1),pre.substr(k));
+		cout << root;
+	 
+	}
+ 
+}
+ 
+int main()
+ 
+{
+	string in,af;
+	cin >> in >> af;
+	fun(in,af);
+	return 0;
+ 
+}
