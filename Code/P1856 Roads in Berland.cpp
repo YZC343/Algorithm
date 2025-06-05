c2c460a4ddb1ed5f2b0d0cbbecfc1e098db8d17c
yzc202403 <akerman233@163.com>
2025-06-05 15:34:35 +0800

    P2035&P1856(notfinish)

---
+++ Code/P1856 Roads in Berland.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<list>
+using namespace std;
+int n,k;
+int w[301][301];
+int r[301][3];
+list<int[3]> p;
+int min_dis() {
+
+}
+int main() {
+    cin>>n;
+    for (int i=1;i<=n;++i) {
+        for (int j=1;j<=n;++j) {
+            cin>>v[i][j];}
+    }
+    cin>>k;
+    for (int i=0;i<k;++i) {
+        cin>>r[i][0]>>r[i][1]>>r[i][2];
+    }
+
+}
--- Code/P2035 星际旅行家.cpp
+++ Code/P2035 星际旅行家.cpp
@@ -1,21 +1,43 @@
 #include<iostream>
 #include<list>
+#include<queue>
 using namespace std;
 list<int> lists[100001];
 int access[100001];
 int main() {
     int n,m;
     cin>>n>>m;
-    for (int i=0;i<=n;++i) {
+    for (int i=0;i<m;++i) {
         int a,b;
         cin>>a>>b;
         lists[a].push_back(b);
         lists[b].push_back(a);
     }
     access[1]=1;
+    queue<int> q;
     for (int v : lists[1]) {
         if (access[v]==0) {
             access[v]=1;
+            q.push(v);
         }
     }
+    while (q.empty()==false) {
+        int i= q.front();
+        for (int v : lists[i]) {
+            if (access[v]==0) {
+                access[v]=1;
+                q.push(v);
+            }
+        }
+        q.pop();
+    }
+
+    int count =0;
+    for (int i=1;i<=n;++i) {
+        if (access[i]==1) {
+            count++;
+        }
+    }
+    cout<<(n-count);
+    return 0;
 }
